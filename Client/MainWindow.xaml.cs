c74c18013ad93fcca4d83a08d941e7fba6e81869
Гребенкин Алексей <24gas2001@gmail.com>
2021-11-07 20:21:12 +0700

    Добавьте файлы проекта.

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    public partial class MainWindow : Window
+    {
+        private const string host = "127.0.0.1";
+        private const int port = 8888;
+
+        TcpClient tcpClient;
+        NetworkStream stream;
+
+        public MainWindow()
+        {
+            tcpClient = new TcpClient();
+            tcpClient.Connect(host, port);
+            stream = tcpClient.GetStream();
+            InitializeComponent();
+            Task.Factory.StartNew(ReciveMessage);
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Task.Factory.StartNew(SendTextBoxValue);
+        }
+
+        private void SendTextBoxValue()
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                SendMessage(textBox1.Text);
+            });
+        }
+
+        private void SendMessage(string message)
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                message = textBox1.Text;
+            });
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            stream.Write(data, 0, data.Length);
+        }
+
+        private void SetTextBoxValue(string message)
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                label1.Content = message;
+            });
+        }
+        private void ReciveMessage()
+        {
+            while (true)
+            {
+                byte[] data = new byte[64];
+                StringBuilder builder = new StringBuilder();
+                do
+                {
+                    int bytes = stream.Read(data, 0, data.Length);
+                    builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                }
+                while (stream.DataAvailable);
+
+                SetTextBoxValue(builder.ToString());
+
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
---
+++ Server/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Server
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+        int port = 8888;
+        TcpListener tcpListener;
+        TcpClient tcpClient;
+        NetworkStream stream;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            tcpListener = new TcpListener(localAddr, port);
+            tcpListener.Start();
+            tcpClient = tcpListener.AcceptTcpClient();
+            stream = tcpClient.GetStream();
+            Task.Factory.StartNew(ReciveMessage);
+        }
+
+        private void AddTextBlockLine(string message)
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                textBlock.Text += message + '\n';
+            });
+        }
+
+        private void ReciveMessage()
+        {
+            while (true)
+            {
+                byte[] data = new byte[64];
+                StringBuilder builder = new StringBuilder();
+                do
+                {
+                    int bytes = stream.Read(data, 0, data.Length);
+                    builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                }
+                while (stream.DataAvailable);
+
+                string message = builder.ToString();
+                AddTextBlockLine(message);
+                SendMessage(message);
+                Thread.Sleep(100);
+            }
+        }
+
+        private void SendMessage(string message)
+        {
+            string response = "Server received: " + message;
+
+            byte[] data = Encoding.Unicode.GetBytes(response);
+            stream.Write(data, 0, data.Length);
+        }
+    }
+}
